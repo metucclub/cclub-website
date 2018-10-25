fee7dec7fdd6d50ad5f85490b8ad7da38db57a00
Ozan Akın <ozn.akn@gmail.com>
2018-10-25 09:14:47 +0300

    add other templates

--- main/urls.py
+++ main/urls.py
@@ -5,6 +5,14 @@ from . import views
 app_name = 'main'
 
 urlpatterns = [
-    url(r'^$', views.home_view, name='home')
+    url(r'^$', views.home_view, name='home'),
+    url(r'^about/$', views.about_view, name='about'),
+    url(r'^announce/$', views.announce_view, name='announce'),
+    url(r'^events/$', views.events_view, name='events'),
+    url(r'^contact/$', views.contact_view, name='contact'),
+    url(r'^bilisimgunu/$', views.bilisim_gunu_view, name='bilisimgunu'),
+    url(r'^bergi/$', views.bergi_view, name='bergi'),
+    url(r'^yarisma/$', views.yarisma_view, name='yarisma'),
+    url(r'^tuzuk/$', views.tuzuk_view, name='tuzuk'),
 ]
 
--- main/views.py
+++ main/views.py
@@ -1,7 +1,34 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+import os
+from django.shortcuts import render, redirect
+from django.http import FileResponse
+from django.contrib.staticfiles.templatetags.staticfiles import static
 
 def home_view(request):
-    return render(request, 'home.html')
+    return render(request, 'pages/home_page.html')
+
+def about_view(request):
+    return render(request, 'pages/about_page.html')
+
+def announce_view(request):
+    return render(request, 'pages/announce_page.html')
+
+def events_view(request):
+    return render(request, 'pages/events_page.html')
+
+def contact_view(request):
+    return render(request, 'pages/contact_page.html')
+
+def bilisim_gunu_view(request):
+    return render(request, 'pages/bilisim_gunu_page.html')
+
+def bergi_view(request):
+    return redirect('https://e-bergi.com')
+
+def yarisma_view(request):
+    return redirect('https://yarisma.cclub.metu.edu.tr')
+
+def tuzuk_view(request):
+    return FileResponse(open('./static/docs/odtu_bilgisayar_toplulugu_tuzugu.pdf', 'rb'), content_type='application/pdf')
