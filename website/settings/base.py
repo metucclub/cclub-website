a443b8188be2acc75434789985ee0e0925a9d983
Ozan Akın <ozn.akn@gmail.com>
2021-05-07 21:24:44 +0300

    set default auto field for django 3.2

--- website/settings/base.py
+++ website/settings/base.py
@@ -115,6 +115,12 @@ USE_L10N = True
 
 USE_TZ = True
 
+# Default primary key field type
+# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+
+
 LOCALE_PATHS = (
     os.path.join(BASE_DIR, '_website', 'locale'),
 )
