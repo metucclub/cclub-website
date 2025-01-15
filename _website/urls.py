bcff1151cfdaaf2747d18ae73bdf45565657b730
mustafa sagopa berke kajmer <ragnogamescorp@gmail.com>
2025-01-16 00:46:25 +0300

    Update urls.py

--- _website/urls.py
+++ _website/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-
+from django.shortcuts import redirect
 from _website.views import *
+
+def arsiv_redirect(request):
+    return redirect('https://www.arsiv.cclub.metu.edu.tr')
 
 urlpatterns = [
     path('', home_view, name='home'),
